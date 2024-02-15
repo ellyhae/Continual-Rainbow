c4e5650d0c372b7f456dec90134a69467c061731
chripp <31983212+ellyhae@users.noreply.github.com>
2024-02-15 10:18:39 +0100

    added reset functionality

--- buffer.py
+++ buffer.py
@@ -1,5 +1,3 @@
-# TODO: implement reset functionality. https://github.com/DLR-RM/stable-baselines3/blob/9cebedc89fcabf509241d89b09bf39fb61cab94b/stable_baselines3/common/buffers.py#L95
-
 import warnings
 from typing import Any, Dict, Generator, List, Optional, Union
 
@@ -28,122 +26,6 @@ except ImportError:
     psutil = None
     
 from utils import prep_observation_for_qnet
-
-#class UniformReplayBuffer(BaseBuffer):
-#    """
-#    Replay buffer used in off-policy algorithms like SAC/TD3.
-#    :param buffer_size: Max number of element in the buffer
-#    :param observation_space: Observation space
-#    :param action_space: Action space
-#    :param device: PyTorch device
-#    :param n_envs: Number of parallel environments
-#    :param optimize_memory_usage: Enable a memory efficient variant
-#        of the replay buffer which reduces by almost a factor two the memory used,
-#        at a cost of more complexity.
-#        See https://github.com/DLR-RM/stable-baselines3/issues/37#issuecomment-637501195
-#        and https://github.com/DLR-RM/stable-baselines3/pull/28#issuecomment-637559274
-#        Cannot be used in combination with handle_timeout_termination.
-#    :param handle_timeout_termination: Handle timeout termination (due to timelimit)
-#        separately and treat the task as infinite horizon task.
-#        https://github.com/DLR-RM/stable-baselines3/issues/284
-#    """
-#
-#    def __init__(
-#        self,
-#        buffer_size: int,
-#        observation_space: spaces.Space,
-#        action_space: spaces.Space,
-#        device: Union[torch.device, str] = "auto",
-#        n_envs: int = 1,
-#        n_step: int = 3,
-#        gamma: float = 0.99,
-#        optimize_memory_usage=False, # ignored
-#    ):
-#        super().__init__(buffer_size, observation_space, action_space, device, n_envs=n_envs)
-#
-#        self.n_step = n_step
-#        self.gamma = gamma
-#
-#        # Check that the replay buffer can fit into the memory
-#        if psutil is not None:
-#            mem_available = psutil.virtual_memory().available
-#
-#        self.observations = np.zeros((self.buffer_size, *self.obs_shape), dtype=observation_space.dtype)
-#        self.next_observations = np.zeros((self.buffer_size, *self.obs_shape), dtype=observation_space.dtype)
-#
-#        self.actions = np.zeros((self.buffer_size, self.action_dim), dtype=action_space.dtype)
-#
-#        self.rewards = np.zeros((self.buffer_size,), dtype=np.float32)
-#        self.dones = np.zeros((self.buffer_size,), dtype=np.float32)
-#
-#        self.n_step_buffers = [deque(maxlen=self.n_step + 1) for j in range(self.n_envs)]
-#
-#        if psutil is not None:
-#            total_memory_usage = self.observations.nbytes + self.next_observations.nbytes + self.actions.nbytes + self.rewards.nbytes + self.dones.nbytes
-#
-#            if total_memory_usage > mem_available:
-#                # Convert to GB
-#                total_memory_usage /= 1e9
-#                mem_available /= 1e9
-#                warnings.warn(
-#                    "This system does not have apparently enough memory to store the complete "
-#                    f"replay buffer {total_memory_usage:.2f}GB > {mem_available:.2f}GB"
-#                )
-#
-#    def add(
-#        self,
-#        obs: np.ndarray,
-#        next_obs: np.ndarray,
-#        action: np.ndarray,
-#        reward: np.ndarray,
-#        done: np.ndarray,
-#        infos: List[Dict[str, Any]],
-#    ) -> None:
-#        # Reshape needed when using multiple envs with discrete observations
-#        # as numpy cannot broadcast (n_discrete,) to (n_discrete, 1)
-#        if isinstance(self.observation_space, spaces.Discrete):
-#            obs = obs.reshape((self.n_envs, *self.obs_shape))
-#            next_obs = next_obs.reshape((self.n_envs, *self.obs_shape))
-#
-#        # Reshape to handle multi-dim and discrete action spaces, see GH #970 #1392
-#        action = action.reshape((self.n_envs, self.action_dim))
-#
-#        for queue, o, a, r, d in zip(self.n_step_buffers, obs, action, reward, done):
-#            queue.append((o, a, r, d))
-#            if len(queue) == self.n_step + 1 and not d:
-#                # get first and last states of the n_step stransition
-#                o, a, r, _ = queue[0]
-#                no, _, _, d = queue[-1]
-#
-#                # calculate reward between
-#                for k in range(1, self.n_step):
-#                    r += queue[k][2] * self.gamma ** k
-#                    if queue[k][3]:
-#                        d = True
-#                        break
-#
-#                self.observations[self.pos] = np.array(o).copy()
-#                self.next_observations[self.pos] = np.array(no).copy()
-#
-#                self.actions[self.pos] = np.array(a).copy()
-#                self.rewards[self.pos] = np.array(r).copy()
-#                self.dones[self.pos] = np.array(d).copy()
-#
-#                self.pos += 1
-#                if self.pos == self.buffer_size:
-#                    self.full = True
-#                    self.pos = 0
-#
-#    def _get_samples(self, batch_inds: np.ndarray, env: Optional[VecNormalize] = None) -> ReplayBufferSamples:
-#
-#        data = (
-#            self._normalize_obs(self.observations[batch_inds, :], env),
-#            self.actions[batch_inds, :],
-#            self._normalize_obs(self.next_observations[batch_inds, :], env),
-#            self.dones[batch_inds].reshape(-1, 1),
-#            self._normalize_reward(self.rewards[batch_inds].reshape(-1, 1), env),
-#        )
-#        return ReplayBufferSamples(*tuple(map(self.to_torch, data)))
     
 
 class PrioritizedReplayBuffer(BaseBuffer):
@@ -182,21 +64,9 @@ class PrioritizedReplayBuffer(BaseBuffer):
         self.n_step = n_step
         self.gamma = gamma
         self.use_amp = use_amp
-
-        # Check that the replay buffer can fit into the memory
-        #if psutil is not None:
-        #    mem_available = psutil.virtual_memory().available
-
-        #self.observations = np.zeros((self.buffer_size, *self.obs_shape), dtype=observation_space.dtype)
-        #self.next_observations = np.zeros((self.buffer_size, *self.obs_shape), dtype=observation_space.dtype)
         
         self.observations = np.full(self.buffer_size, None)
         self.next_observations = np.full(self.buffer_size, None)
-
-        #self.actions = np.zeros((self.buffer_size, self.action_dim), dtype=action_space.dtype)
-
-        #self.rewards = np.zeros((self.buffer_size,), dtype=np.float32)
-        #self.dones = np.zeros((self.buffer_size,), dtype=np.float32)
         
         self.actions = torch.zeros((self.buffer_size, self.action_dim), dtype=torch.int64, device=self.device)
         
@@ -207,23 +77,8 @@ class PrioritizedReplayBuffer(BaseBuffer):
 
         self.priority_sum = np.full((2 * self.buffer_size), 0., dtype=np.float64)
         self.priority_min = np.full((2 * self.buffer_size), np.inf, dtype=np.float64)
-        
-        #self.priority_sum = [0 for _ in range(2 * self.buffer_size)]
-        #self.priority_min = [float('inf') for _ in range(2 * self.buffer_size)]
 
         self.max_priority = 1.0  # initial priority of new transitions
-
-        # not very useful, as observations are stored in a memory efficient way
-        #if psutil is not None:
-        #    total_memory_usage = self.observations.nbytes + self.next_observations.nbytes + self.actions.nbytes + self.rewards.nbytes + self.dones.nbytes
-        #    if total_memory_usage > mem_available:
-        #        # Convert to GB
-        #        total_memory_usage /= 1e9
-        #        mem_available /= 1e9
-        #        warnings.warn(
-        #            "This system does not have apparently enough memory to store the complete "
-        #            f"replay buffer {total_memory_usage:.2f}GB > {mem_available:.2f}GB"
-        #        )
 
     def add(
         self,
@@ -343,3 +198,13 @@ class PrioritizedReplayBuffer(BaseBuffer):
             self.rewards[batch_inds].reshape(-1, 1),
         )
         return ReplayBufferSamples(*data)
+    
+    def reset(self):
+        super().reset()  # reset pos and full. Causes e.g. observations to be automatically overwritten, so nned to reset them
+        
+        self.n_step_buffers = [deque(maxlen=self.n_step + 1) for j in range(self.n_envs)]
+
+        self.priority_sum = np.full((2 * self.buffer_size), 0., dtype=np.float64)
+        self.priority_min = np.full((2 * self.buffer_size), np.inf, dtype=np.float64)
+
+        self.max_priority = 1.0
